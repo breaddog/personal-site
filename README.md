3891f5fa175024f0e0a5a17cda26372bea96dea1
Tien Foong <46518750+breaddog@users.noreply.github.com>
2023-03-27 23:25:46 +1100

    init repo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# personal-site
- 
+# Personal Site
+
+For fun + portfolio
---
+++ src/react-app-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react-scripts" />
+// declare module '*.module.scss'
+// declare module '*.jpg'
+// declare module '*.png'
+// declare module '*.jpeg'
+// declare module '*.gif'
+// declare module '*.mp4'
