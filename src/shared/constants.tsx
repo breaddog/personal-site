4c0240b62a5393f59307b670221360da68fd7ec9
breaddog <46518750+breaddog@users.noreply.github.com>
2023-06-14 03:20:10 +1000

    fix: document, image path

--- src/shared/constants.tsx
+++ src/shared/constants.tsx
@@ -2,7 +2,8 @@ import { APP_ENV } from '../config'
 
 const { IS_PROD } = APP_ENV
 export const CONSTANTS = {
-  projectImageRoot: `${IS_PROD ? '/personal-site' : '/'}images/projects`,
+  projectFileRoot: `${IS_PROD ? '/personal-site' : ''}`,
+  projectImageRoot: `${IS_PROD ? '/personal-site' : ''}/images/projects`,
   mobileMediaQuery: '(max-width: 876px)',
   mediumMediaQuery: '(max-width: 1280px)',
 }
--- src/shared/links.tsx
+++ src/shared/links.tsx
@@ -1,8 +1,11 @@
+import { CONSTANTS } from './constants'
+const { projectFileRoot } = CONSTANTS
+
 export const EXTERNAL_LINKS = {
   linkedin: 'https://www.linkedin.com/in/tien-foong-leong-627808177/',
   github: 'https://github.com/breaddog/',
   email: 'tienfoong@gmail.com',
-  resume: './documents/resume.pdf',
+  resume: `${projectFileRoot}/documents/resume.pdf`,
   metamask: {
     download: 'https://metamask.io/download/',
     main: 'https://metamask.io/',
