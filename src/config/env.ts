41160afb047b7c9601e70fee6f39d447b6582b98
breaddog <tienfoong@gmail.com>
2026-02-25 00:21:27 +1100

    chore: rename REACT_APP to VITE

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,20 +1,20 @@
 export const APP_ENV = {
-  IS_PROD: process.env.REACT_APP_IS_PROD || false,
+  IS_PROD: process.env.VITE_IS_PROD || false,
   ETHEREUM_PROVIDER: String(
-    process.env.REACT_APP_ETHEREUM_PROVIDER || 'https://rpc.ankr.com/eth'
+    process.env.VITE_ETHEREUM_PROVIDER || 'https://rpc.ankr.com/eth'
   ),
   POLYGON_PROVIDER: String(
-    process.env.REACT_APP_POLYGON_PROVIDER || 'https://rpc.ankr.com/polygon'
+    process.env.VITE_POLYGON_PROVIDER || 'https://rpc.ankr.com/polygon'
   ),
-  MAINNET_CHAIN_ID: Number(process.env.REACT_APP_MAINNET_CHAIN_ID || 1),
-  POLYGON_CHAIN_ID: Number(process.env.REACT_APP_POLYGON_CHAIN_ID || 137),
+  MAINNET_CHAIN_ID: Number(process.env.VITE_MAINNET_CHAIN_ID || 1),
+  POLYGON_CHAIN_ID: Number(process.env.VITE_POLYGON_CHAIN_ID || 137),
   CONNECTOR_TYPE_STORAGE: String(
-    process.env.REACT_APP_CONNECTOR_TYPE_STORAGE || 'CONNECTOR_TYPE'
+    process.env.VITE_CONNECTOR_TYPE_STORAGE || 'CONNECTOR_TYPE'
   ),
   SCROLL_POSITION_STORAGE: String(
-    process.env.REACT_APP_SCROLL_POSITION_STORAGE || 'SCROLL_POSITION'
+    process.env.VITE_SCROLL_POSITION_STORAGE || 'SCROLL_POSITION'
   ),
   PAGE_LOAD_CHECK_STORAGE: String(
-    process.env.REACT_APP_PAGE_LOAD_CHECK_STORAGE || 'PAGE_LOAD_CHECK'
+    process.env.VITE_PAGE_LOAD_CHECK_STORAGE || 'PAGE_LOAD_CHECK'
   ),
 }
